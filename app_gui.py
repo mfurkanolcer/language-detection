8e667282abad21bed1d450330fdc34ce2275a77f
M Furkan Olcer <muhammedfurkanolcer@hotmail.com>
2024-05-09 22:13:19 +0300

    update gui and add flag image

--- app_gui.py
+++ app_gui.py
@@ -4,6 +4,7 @@ import numpy as np
 import csv
 from PIL import Image, ImageTk
 
+# Frekansları okumak için CSV dosyasından verileri okuyan fonksiyon
 def read_frequencies(filename):
     bigram_strings = []
     trigram_strings = []
@@ -20,13 +21,16 @@ def read_frequencies(filename):
 
     return bigram_strings, trigram_strings, bigram_frequencies, trigram_frequencies
 
+# Her dil için frekans verilerini yükleyen fonksiyon
 def load_language_data(language):
     bigram_strings, trigram_strings, frequency_bigram, frequency_trigram = read_frequencies(f'./csv/{language.lower()}.csv')
     return bigram_strings, trigram_strings, frequency_bigram, frequency_trigram
 
+# Desteklenen diller ve dil verilerini depolayan sözlük
 languages = ["english", "german", "french", "spanish", "portuguese", "italian", "turkish"]
 language_data = {}
 
+# Dil verilerini yükleyerek language_data sözlüğüne ekleyen döngü
 for language in languages:
     bigram_strings, trigram_strings, frequency_bigram, frequency_trigram = load_language_data(language)
     language_data[language.upper()] = {
@@ -36,14 +40,18 @@ for language in languages:
         "FREQUENCY_TRIGRAM": frequency_trigram
     }
 
-frequency_eng = language_data["ENGLISH"]["FREQUENCY_BIGRAM"] + language_data["ENGLISH"]["FREQUENCY_TRIGRAM"]
-frequency_germ = language_data["GERMAN"]["FREQUENCY_BIGRAM"] + language_data["GERMAN"]["FREQUENCY_TRIGRAM"]
-frequency_french = language_data["FRENCH"]["FREQUENCY_BIGRAM"] + language_data["FRENCH"]["FREQUENCY_TRIGRAM"]
-frequency_spanish = language_data["SPANISH"]["FREQUENCY_BIGRAM"] + language_data["SPANISH"]["FREQUENCY_TRIGRAM"]
-frequency_portuguese = language_data["PORTUGUESE"]["FREQUENCY_BIGRAM"] + language_data["PORTUGUESE"]["FREQUENCY_TRIGRAM"]
-frequency_italian = language_data["ITALIAN"]["FREQUENCY_BIGRAM"] + language_data["ITALIAN"]["FREQUENCY_TRIGRAM"]
-frequency_turkish = language_data["TURKISH"]["FREQUENCY_BIGRAM"] + language_data["TURKISH"]["FREQUENCY_TRIGRAM"]
+# Küçük harfe çevrilmiş dil isimleri
+languages_lower = [lang.lower() for lang in languages]
 
+# Her bir dilin toplam bigram ve trigram frekanslarını hesaplayan döngü
+for language in languages_lower:
+    frequency_bigram = language_data[language.upper()]["FREQUENCY_BIGRAM"]
+    frequency_trigram = language_data[language.upper()]["FREQUENCY_TRIGRAM"]
+    frequency_total = frequency_bigram + frequency_trigram
+    globals()[f"frequency_{language}"] = frequency_total
+
+
+# Metindeki bigram frekanslarını hesaplayan fonksiyon
 def calculate_frequencies_bi(string, language):
     frequencies = [0] * len(language_data[language]["BIGRAM_STRINGS"])
     for i in range(len(string) - 1):
@@ -53,6 +61,7 @@ def calculate_frequencies_bi(string, language):
             frequencies[index] += 1
     return frequencies
 
+# Metindeki trigram frekanslarını hesaplayan fonksiyon
 def calculate_frequencies_tri(string, language):
     frequencies = [0] * len(language_data[language]["TRIGRAM_STRINGS"])
     for i in range(len(string) - 2):
@@ -62,16 +71,19 @@ def calculate_frequencies_tri(string, language):
             frequencies[index] += 1
     return frequencies
 
+# Metin frekanslarına göre uzaklığı hesaplayan fonksiyon
 def calculate_distances(frequencies, language):
     frequency_ref = language_data[language]["FREQUENCY_BIGRAM"] + language_data[language]["FREQUENCY_TRIGRAM"]
     distance = np.dot(frequency_ref, frequencies)
     return distance
 
+# En büyük uzaklığa sahip dili tespit eden fonksiyon
 def detect_lang(distances):
     max_distance = max(distances.values())
     language = [lang for lang, dist in distances.items() if dist == max_distance][0]
     return language
 
+# Tespit edilen dilin bayrağını gösteren fonksiyon
 def display_language_image(language):
     if language.upper() in language_data:
         image_path = f"img/{language.lower()}.png"
@@ -80,6 +92,7 @@ def display_language_image(language):
         flag_label.configure(image=image)
         flag_label.image = image
 
+# Dil tespiti işlemini gerçekleştiren fonksiyon
 def run_language_detection():
     text = text_box.get("1.0",'end-1c').strip()
     if not text:
@@ -102,26 +115,29 @@ def run_language_detection():
 
     detected_language = detect_lang(distances)
 
-    result_text += f"\nLANGUAGE: {detected_language}\n\n"
+    result_text += f"\nLANGUAGE ---> {detected_language}\n\n"
     result_box.delete('1.0', tk.END)
     result_box.insert(tk.END, result_text)
     display_language_image(detected_language)
 
+# Metni temizleyen fonksiyon
 def filter_str(string):
     filtered_str = ''.join([char.upper() if char.isalpha() else ' ' for char in string])
     return filtered_str
 
+# Metin kutusunu ve sonuç kutusunu sıfırlayan fonksiyon
 def reset_text():
     text_box.delete('1.0', tk.END)
     result_box.delete('1.0', tk.END)
     flag_label.configure(image='')
 
+# GUI oluşturan fonksiyon
 def create_gui():
     window = tk.Tk()
     window.title("Language Detection")
 
-    window_width = 1050
-    window_height = 520
+    window_width = 1150
+    window_height = 550
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
     x = (screen_width - window_width) // 2
@@ -132,11 +148,11 @@ def create_gui():
     text_label.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
     global text_box
-    text_box = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=45, height=20)
+    text_box = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=55, height=20)
     text_box.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
     run_button = tk.Button(window, text="RUN", command=run_language_detection)
-    run_button.grid(row=2, column=0, padx=200, pady=10, sticky="w")
+    run_button.grid(row=2, column=0, padx=200, pady=20, sticky="w")
 
     result_label = tk.Label(window, text="Result:")
     result_label.grid(row=0, column=1, padx=10, pady=5, sticky="w")
@@ -147,7 +163,7 @@ def create_gui():
     result_box.configure(bg=window.cget('bg'))
 
     reset_button = tk.Button(window, text="RESET", command=reset_text)
-    reset_button.grid(row=2, column=1, padx=10, pady=5, sticky="e")
+    reset_button.grid(row=2, column=1, padx=1, pady=20, sticky="e")
 
     global flag_label
     flag_label = tk.Label(window)
@@ -155,4 +171,5 @@ def create_gui():
 
     window.mainloop()
 
+# GUI oluştur
 create_gui()
